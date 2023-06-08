3a4b40b590f18bb4642f445e69740d6788ae2f87
vutl <vu.tran10100305@gmail.com>
2023-06-08 15:52:27 +0700

    remove stt module

--- lec_chathelper/stt_interface/api/v1/general/faster_whisper/utils.py
+++
@@ -1,9 +0,0 @@
-from ctranslate2.converters.transformers import TransformersConverter
-
-## MODEL RELATING
-def model_converter(model, model_output):
-    converter = TransformersConverter("openai/whisper-" + model)
-    try:
-        converter.convert(model_output, None, "float16", False)
-    except Exception as e:
-        print(e)
--- lec_chathelper/stt_interface/api/v1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path, include
-
-
-urlpatterns = [
-    path('general/', include("stt_interface.api.v1.general.urls")),
-]
